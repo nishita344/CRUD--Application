7afd65289875c02fd47678bd26744286a50cbdc9
nishita344 <nishita344@gmail.com>
2026-05-30 20:27:19 +0530

    remove db, use localStorage and sessionStorage

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
 
-const userRoutes = require("./routes/users");
-
-app.use("/users", userRoutes);
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
